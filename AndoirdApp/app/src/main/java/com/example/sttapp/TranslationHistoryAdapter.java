b42420029a959c0bab8358ad22b9590b096945ed
phamtaiduc <77768475+phamtaiduc@users.noreply.github.com>
2024-11-12 17:09:35 +0700

    Update TranslationHistory

--- AndoirdApp/app/src/main/java/com/example/sttapp/TranslationHistoryAdapter.java
+++ AndoirdApp/app/src/main/java/com/example/sttapp/TranslationHistoryAdapter.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
+ 
 import java.util.List;
 
 public class TranslationHistoryAdapter extends RecyclerView.Adapter<TranslationHistoryAdapter.HistoryViewHolder> {
