5a33bae9e7ca0eca7b1b20f07a9366eb5c3a249f
phamtaiduc <77768475+phamtaiduc@users.noreply.github.com>
2024-11-12 17:09:19 +0700

    Update SpeechHistory

--- AndoirdApp/app/src/main/java/com/example/sttapp/SpeechHistoryAdapter.java
+++ AndoirdApp/app/src/main/java/com/example/sttapp/SpeechHistoryAdapter.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
+ 
 import java.util.List;
 
 public class SpeechHistoryAdapter extends RecyclerView.Adapter<SpeechHistoryAdapter.HistoryViewHolder> {
